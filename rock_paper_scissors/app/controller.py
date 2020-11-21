c9129aa98e0d1b6b1c271c4ef5c58f6345243ae9
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-21 14:24:40 +0000

    Created page to show winner from url-based moves

---
+++ rock_paper_scissors/app/controller.py
@@ -0,0 +1,18 @@
+from flask import render_template, request, redirect
+from app import app
+from app.models.game import Game
+from app.models.player import Player
+
+@app.route('/')
+def index():
+    # normal python function
+    return render_template('index.html', title='Rock, Paper, Scissors')
+
+@app.route('/<move1>/<move2>')
+def play_a_game(move1, move2):
+    player_1 = Player( "Player 1", move1 )
+    player_2 = Player( "Player 2", move2 )
+    game = Game( player_1, player_2 )
+    winner = game.play_game( player_1, player_2 )
+    return (render_template("winner.html", winner = winner, move1=move1, move2 =move2))
+
